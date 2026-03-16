5cf4234ea9fca706f3096175c085f8e5446be681
samtari1 <samtariyang@gmail.com>
2026-03-15 20:16:34 -0400

    Initial Commit

---
+++ ImageAnalysis/ImageAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAnalysisApp.swift
+//  ImageAnalysis
+//
+//  Created by Quanpeng Yang on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ImageAnalysisTests/ImageAnalysisTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAnalysisTests.swift
+//  ImageAnalysisTests
+//
+//  Created by Quanpeng Yang on 3/15/26.
+//
+
+import Testing
+@testable import ImageAnalysis
+
+struct ImageAnalysisTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
